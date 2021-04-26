501a54e18992cc9aa6c12af1058a7431207ec811
Felix <ofelix@pm.me>
2021-04-26 16:38:35 +0200

    Favicons, fixed vue router error, fixed vue path

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import 'buefy/dist/buefy.css'
 
 Vue.use(Buefy);
+Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: new VueRouter()
 }).$mount('#app')
