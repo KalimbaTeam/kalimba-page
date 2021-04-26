b5d21673b3d3623927aad8ac399c2d1ab201c302
felkr <5049120+felkr@users.noreply.github.com>
2021-04-26 13:38:15 +0000

    Deploying to gh-pages from @ KalimbaTeam/kalimba-page@7af7e6416b235df5722bddcbf4899e55b9d9aef9 🚀

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "./components/home";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+<style>
+.main-hero {
+  background-image: url("https://pixabay.com/get/g0a60abe83efe26c3e40e64e1afb443b59e2daf6c7c26f1888d7f7d609bdae982e3c1c6c4da7839ed835b549ca446a6f1_1920.jpg");
+  background-size: cover;
+  backdrop-filter: blur(20px);
+}
+</style>
